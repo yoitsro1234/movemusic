3fb41c28dda0b36398b14f2ddd8ab205f9730e61
yoitsro1234 <72605391+yoitsro1234@users.noreply.github.com>
2021-04-29 15:43:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+song = "";
+function setup(){
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide;
+}
+
+function draw(){
+    image(video, 0, 0, 600, 500);
+}
+
+function preload(){
+
+}
+
+function play(){
+    song.play();
+}
