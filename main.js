d8d83c226911c587c39d736da04a3e1adf3e6308
yoitsro1234 <72605391+yoitsro1234@users.noreply.github.com>
2021-04-30 15:51:37 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 song = "";
+leftWristY = 0;
+leftWristX = 0;
+rightWristY = 0;
+rightWristX = 0;
+
 function setup(){
     canvas = createCanvas(600, 500);
     canvas.center();
 
     video = createCapture(VIDEO);
     video.hide;
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
 function draw(){
@@ -17,4 +25,24 @@ function preload(){
 
 function play(){
     song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
+
+function modelLoaded(){
+    console.log("MODEL LOADED LETS GOOOOOOOOOOOO");
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("Left wrist X = "+leftWristX+", Left wrist Y = "+leftWristY);
+
+        rightWristY = results[0].pose.rightWrist.y;
+        rightWristX = results[0].pose.rightWrist.x;
+        console.log("Right wrist X = "+rightWristX+", Right wrist Y = "+rightWristY);
+    }
+}
+
